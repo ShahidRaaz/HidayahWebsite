4917a17d5c0309021a2d7e2f552cabca8b3195fa
Shahid <mdshaaz712@gmail.com>
2025-09-13 12:38:55 +0530

    update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ import OTA from "./OTA";
 import Work from "./Works";
 import AH from "./AboutH";
 import WACard from "./WACard";
-import FBA from "../../components/Fba"
+// import FBA from "../../components/Fba"
 
 const Home = () => 
 <div className="overflow-y-hidden mt-[25px] md:mt-[50px] lg:mt-[50px] xl:mt-[75px] w-full flex flex-col gap-4 lg:gap-24 justify-center items-center">
@@ -14,7 +14,7 @@ const Home = () =>
     <OTA/>
     <Work/>
     <WACard/>
-    <FBA/>
+    {/* <FBA/> */}
     
 
 </div>;
